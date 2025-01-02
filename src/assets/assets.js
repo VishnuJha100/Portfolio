0725001598661367d3512acd3dd9c217812ab5c7
VishnuJha100 <vishnujha100@gmail.com>
2025-01-02 11:47:41 +0530

    created navbar and hero section

---
+++ src/assets/assets.js
@@ -0,0 +1,23 @@
+import linkedin_logo from './linkedin.png'
+import x_logo from './x.png'
+import logo_icon from './logo.png'
+import hero from './hero.png'
+import react_logo from './react.png'
+import nodejs_logo from './nodejs.png'
+import tailwind_logo from './tailwind.png'
+import instagram_logo from './instagram.png'
+import facebook_logo from './facebook.png'
+
+
+export const assets = {
+    linkedin_logo,
+    x_logo,
+    logo_icon,
+    hero,
+    react_logo,
+    tailwind_logo,
+    nodejs_logo,
+    instagram_logo,
+    facebook_logo,
+}
+
---
+++ src/component/Hero.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Hero = () => {
+  return (
+    <section>
+      <div className='max-w-7xl mx-auto'>
+        <div className='flex flex-col md:flex-row items-center lg:h-[90vh] justify-between'>
+            <div className='md:w-[1/2] mb:8 md:mb-0 flex flex-col space-y-4 px-6 lg:px-0 lg:mt-0 mt-10'>
+                <h1 className='lg:text-7xl text-4xl font-bold lg:leading-snug'>Hi there, <br/>I'm Vishnu <span className='text-red-500'>Jha</span></h1>
+                <p className='md:text-2xl text-xl mb-4'>Full stack developer</p>
+                <p className='mb:4'>I'm a passionate web developer with experties in React, Express.js and modern web technologies. I love creating beautiful and functional website that solve real world problems.</p>
+                <button className='text-white px-3 bg-black py-2 w-max rounded-md'>Download CV</button>
+            </div>
+            <div className='md:w-1/2 relative flex justify-center item-end'>
+                <img src={assets.hero} alt="" />
+                <img className='w-10 absolute md:hidden top-36 left-0 rounded-full' src={assets.react_logo} alt="" />
+                <img className='w-10 absolute top-0 right-5 md:hidden' src={assets.nodejs_logo} alt="" />
+                <img className='w-10 absolute rounded-full right-0 bottom-36 md:hidden' src={assets.tailwind_logo} alt="" />
+            </div>
+        </div>
+      </div>
+      <div className='absolute top-44 right-8 hidden bg-gray-200 p-4 md:flex flex-col gap-6 rounded-full'>
+        <img className='w-20' src={assets.linkedin_logo} alt="" />
+        <img className='w-20' src={assets.instagram_logo} alt="" />
+        <img className='w-20' src={assets.x_logo} alt="" />
+        <img className='w-20' src={assets.facebook_logo} alt="" />
+      </div>
+    </section>
+  )
+}
+
+export default Hero
